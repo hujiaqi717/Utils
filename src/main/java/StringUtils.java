ac586846a9d3d8db160366ec4528bfe359d5d74e
HuJiaqi <943490955@qq.com>
2016-04-14 10:29:04 +0800

    添加了两个工具类

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -3,5 +3,20 @@
  * @createTime 2016年04月13日 11时57分
  * @discription String工具类
  */
+@SuppressWarnings("unused")
 public class StringUtils {
+
+    /**
+     * @auther HuJiaqi
+     * @createTime 2016年04月14日 10时28分
+     * @discription 对象是否为null或者""，仅用于可包装成String的对象
+     */
+    public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        String str = String.valueOf(obj);
+        return (("".equals(str)) || (str == null));
+    }
+
 }
